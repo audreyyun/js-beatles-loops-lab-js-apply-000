b44841f83ed35db4e8fdb711ae477eb5ea961b9a
Audrey Yun <96audreyyun@gmail.com>
2020-09-16 21:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments) {
+  var arr = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts (facts) {
+  var exclArr = [];
+  
+  var i = 0;
+  while (i < facts.length) {
+    var fact = facts[i]; 
+    exclArr.push(fact +'!!!');
+  }
+  i++;
+  
+  return exclArr;
+}
